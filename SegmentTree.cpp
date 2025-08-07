9fdaa432d0afa43ae442c3d6d1b0c21d50f1682e
Faheem Ul Islam <99001563+BhatFaheem@users.noreply.github.com>
2025-08-07 11:01:49 +0530

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,46 +1,67 @@
-
-class SGTree {
-	vector<int>seg;
+struct Node{
+    int max_ind = -1;
+    int min_ind = INF;
+};
+vector<Node>segtree;
+class SegmentTree {
 public:
-	SGTree(int n)
-	{
-		seg.resize(4 * n + 1);
-	}
-	void build(int ind , int low , int high , int arr[])
-	{
-		if (low == high)
-		{
-			seg[ind] = arr[low];
-			return;
-		}
-		int mid = (high + low) / 2;
-		build(2 * ind + 1 , low , mid , arr);
-		build(2 * ind + 2 , mid + 1 , high , arr);
-		seg[ind] = min(seg[2 * ind + 1] , seg[2 * ind + 2]);
-	}
-
-	int query(int ind , int low , int high, int l , int r)
-	{
-		if (r < low ||  l > high) return INT_MAX;
-		if (low >= l && high <= r) return seg[ind];
-
-		int mid = (low + high) >> 1;
-
-		int left = query(2 * ind + 1 , low , mid , l , r);
-		int right = query(2 * ind + 2 , mid + 1 , high , l , r);
-		return min(left , right);
-	}
-
-	void update(int ind , int low , int high , int i , int val)
-	{
-		if (high == low)
-		{
-			seg[ind] = val;
-			return;
-		}
-		int mid = (high + low) / 2;
-		if (i <= mid)update(2 * ind + 1 , low , mid , i , val);
-		else update(2 * ind + 2 , mid + 1 , high , i , val);
-		seg[ind] = min(seg[2 * ind + 1] , seg[2 * ind + 2]);
-	}
+    int n;
+    SegmentTree(int n)
+    {
+        this->n = n;
+        segtree.resize(4 * n + 1);
+    }
+    Node merge(Node & a , Node & b)
+    {
+        Node node;
+        node.max_ind = max(a.max_ind , b.max_ind);
+        node.min_ind = min(a.min_ind , b.min_ind);
+        return node;
+    }
+    void build(vector<int>& arr , int start , int end , int ind)
+    {
+        if (start == end)
+        {
+            segtree[ind].min_ind = segtree[ind].max_ind = start;
+            return;
+        }
+        int left = 2 * ind , right = 2 * ind + 1;
+        int mid = (start + end) / 2;
+        build(arr , start , mid , left);
+        build(arr , mid + 1 , end , right);
+        segtree[ind] = merge(segtree[left] , segtree[right]);
+    }
+    void update(int start , int end , int ind , int pos)
+    {
+        if (start == end)
+        {
+            segtree[ind].max_ind = -1;
+            segtree[ind].min_ind = INF;
+            return;
+        }
+        int left = 2 * ind , right = 2 * ind + 1;
+        int mid = (start + end) / 2;
+        if (mid >= pos) update(start , mid , left , pos );
+        else update(mid + 1 , end , right ,  pos );
+        segtree[ind] = merge(segtree[left] , segtree[right]);
+    }
+    Node query(int start  , int end , int ind , int l , int r)
+    {
+        if (start >= l && end <= r) return segtree[ind];
+        if (l > end || r < start) return Node();
+        int mid = (start + end) / 2;
+        Node leftans = query(start , mid , 2 * ind , l , r);
+        Node rightans = query(mid + 1 ,  end , 2 * ind + 1 , l , r);
+        return merge(leftans , rightans);
+    }
+    void build(vector<int>& arr){
+        build(arr , 0 , n-1 , 1);
+    }
+    void update(int pos){
+        update(0 , n-1 , 1 , pos);
+    }
+    Node query(int l , int r){
+        return query(0 , n-1 , 1 , l , r);
+    }
+ 
 };
